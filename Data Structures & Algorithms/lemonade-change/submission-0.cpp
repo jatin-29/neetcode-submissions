4e7e33aa337723f1b0315d4aa14708f7c07e6fa5
jatin-29 <165682990+jatin-29@users.noreply.github.com>
2026-07-30 08:42:35 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int five=0;
+        int ten=0;
+
+        for(int bill : bills){
+            if(bill==5)
+            five++;
+
+
+            else if (bill==10){
+                if(five<1) return false;
+                ten++;
+                five--;
+            }else{
+                // bill==20
+
+                if(five>=1 && ten>=1){
+                    five--;
+                    ten--;
+                } else if(five>=3){
+                    five-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
